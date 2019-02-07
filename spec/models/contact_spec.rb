905b055e1a3b2f440cf26d6572e3a2755668ab2e
Anup Singh <anup.singh2071@gmail.com>
2019-02-07 21:40:53 +0545

    Replaced Phone.rb with FactoryGirls methods

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,40 +2,40 @@ require 'rails_helper'
 
 describe Contact do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:contact)).to be_valid
+    expect(build(:contact)).to be_valid
   end
 
   it "is valid with a firstname, lastname and email" do
-    contact = FactoryGirl.build(:contact)
+    contact = build(:contact)
     expect(contact).to be_valid
   end
 
   it "is invalid without a firstname" do
-    contact = FactoryGirl.build(:contact, firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   it "is invalid without a lastname" do
-    contact = FactoryGirl.build(:contact, lastname: nil)
+    contact = build(:contact, lastname: nil)
     expect(contact).not_to be_valid
   end
 
   it "is invalid without an email address" do
-    contact = FactoryGirl.build(:contact, email: nil)
+    contact = build(:contact, email: nil)
     expect(contact).not_to be_valid
   end
 
   it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:contact, email: 'aaron@example.com')
-    contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
+    create(:contact, email: 'aaron@example.com')
+    contact = build(:contact, email: 'aaron@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
   # Testing instance methods
   it "returns a contact's full name as a string" do
-    contact = FactoryGirl.build(:contact,
+    contact = build(:contact,
     firstname: "Jane",
     lastname: "Smith"
     )
@@ -45,17 +45,17 @@ describe Contact do
   # Testing class methods and scopes
   describe "filter last name by letter" do
     before :each do
-      @smith = FactoryGirl.create(:contact,
+      @smith = create(:contact,
       firstname: "Jane",
       lastname: "Smith"
       )
 
-      @jones = FactoryGirl.create(:contact,
+      @jones = create(:contact,
       firstname: "Tim",
       lastname: "Jones"
       )
 
-      @johnson = FactoryGirl.create(:contact,
+      @johnson = create(:contact,
       firstname: "John",
       lastname: "Johnson"
       )
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,44 +2,24 @@ require 'rails_helper'
 
 describe Phone do
   it "does not allow duplicate phone numbers per contact" do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-    contact.phones.create(
-      phone_type: 'home',
+    contact = create(:contact)
+    create(:home_phone,
+      contact: contact,
       phone: '785-555-1234'
-    )
-    mobile_phone = contact.phones.build(
-      phone_type: 'mobile',
-      phone: '785-555-1234'
-    )
+      )
 
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
+      phone: '785-555-1234'
+      )
     expect(mobile_phone).not_to be_valid
   end
 
   it "allows two contacts to share a phone number" do
-    contact1 = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-    contact2 = Contact.create(
-      firstname: 'San',
-      lastname: 'Chester',
-      email: 'chester@example.com'
-    )
-
-    c1_phone = contact1.phones.create(
-        phone_type: 'mobile',
-        phone: '785-555-1234'
-    )
-    c2_phone = contact2.phones.build(
-        phone_type: 'mobile',
-        phone: '785-555-1234'
-    )
-    expect(c2_phone).to be_valid
+    create(:home_phone,
+      phone: '785-555-1234'
+      )
+    expect(build(:home_phone, phone: '785-555-1234')).to be_valid
   end
 
 end
